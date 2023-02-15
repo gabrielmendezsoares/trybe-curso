60271f36bd9669a56efae09cc5ecc2206661672d
Gabriel Mendez Soares <123321309+gabrielmendezsoares@users.noreply.github.com>
2023-02-14 22:30:44 -0300

    Update exercicio-01.sh

--- fundamentos/secao-01-unix-shell-e-git/dia-01-unix-e-shell/exercicio-01.sh
+++ fundamentos/secao-01-unix-shell-e-git/dia-01-unix-e-shell/exercicio-01.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Made by: Gabriel Mendez Soares
+# Feito por : Gabriel Mendez Soares
+
 mkdir unix_tests
 cd unix_tests
 touch trybe.txt
 cp trybe.txt trybe_backup.txt
-mv trybe.txt outro_nome.txt
+mv trybe.txt default_name.txt
 mkdir backup
 mv trybe_backup backup
 mkdir backup2
@@ -18,8 +21,7 @@ clear
 touch skills.txt
 cat > skills.txt
 
-# ---//
-
+# ---// Insertion Log //
 Internet
 Unix
 Bash
@@ -28,8 +30,7 @@ CSS
 JavaScript
 React
 SQL
-
-# ---//
+# ---// Insertion Log //
 
 head -n 5 skills.txt
 tail -n 4 skills.txt
