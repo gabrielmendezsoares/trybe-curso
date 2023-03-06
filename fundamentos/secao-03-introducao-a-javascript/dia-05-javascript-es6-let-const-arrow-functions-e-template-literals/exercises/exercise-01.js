b71d0d495f4051ba4dd440d4ec0d828596e5f392
Gabriel Mendez Soares <gabrielmendezsoares@gmail.com>
2023-03-05 21:36:39 -0300

    v1.4

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/exercises/exercise-01.js
@@ -0,0 +1,35 @@
+let firstName = 'João';
+let lastName = 'Silva';
+let favoriteFood = "McDonald's";
+const valueA = 3;
+const valueB = 5;
+const sumNumber = (A, B) => A + B;
+const randomNumber = () => Math.random();
+const getHello = (string) => `Olá, ${string}!`;
+const getSpeed = (speed) => (speed >= 120) ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
+const getName = (A, B) => `${A} ${B}`;
+const getAge = (A, B) => {
+  for (let idade = A; idade <= B; idade += 1) {
+    console.log('Idade:', idade);
+  } 
+};
+const pessoa = {
+  nome: 'Ninguém',
+  idade: 0,
+};
+
+getAge(1, 18);
+firstName = 'Gabriel';
+lastName = 'Soares';
+favoriteFood = 'Burger King';
+pessoa.nome = 'Gabriel';
+pessoa.idade = 18;
+console.log(getSpeed(180));
+console.log(getName('Ricardo', 'Batista'));
+console.log(getHello('Marcos Silva'));
+console.log(`Olá, ${firstName} ${lastName}!`);
+console.log(`Nome: ${pessoa.nome}`);
+console.log(`Idade: ${pessoa.idade}`);
+console.log(`Comida: ${favoriteFood}`);
+console.log(`Resultado da soma: ${sumNumber(valueA, valueB)}`);
+console.log(`Número aleatório: ${randomNumber()}`);
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/exercises/exercise-02.js
@@ -0,0 +1,24 @@
+const stringInserter = (string) => {
+  const word = 'Tryber x aqui!';
+  const wordArray = word.split(' ');
+  for (let index = 0; index < wordArray.length; index += 1) {
+    if (wordArray[index] === 'x') {
+      wordArray[index] = string;
+    }
+  }  
+  return wordArray.join(' ');
+};
+const stringFormatter = (callback) => {
+  const skills = ['JavaScript', 'HTML', 'CSS'];
+  let string = `
+  ${callback}
+  
+  Minhas três principais habilidades são:`;
+  for (let index = 0; index < skills.length; index += 1) {
+    string = `${string}
+    -> ${skills[index]}`;
+  }
+  return string;
+};
+  
+console.log(stringFormatter(stringInserter('Gabriel')));
