a7551b81a8094090fdc18661ebeed91e05f962ed
Gabriel Mendez Soares <gabrielmendezsoares@gmail.com>
2023-03-21 07:33:10 -0300

    v4.1

--- bloco-01-fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-frameworks-css/exercicio-03.js
+++ bloco-01-fundamentos/secao-05-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-frameworks-css/exercicio-03.js
@@ -21,27 +21,33 @@ const validate = new JustValidate(
 function clearFields() {
   const formElements = document.querySelectorAll('input');
   const textArea = document.querySelector('textarea');
+  
   for (let index = 0; index < formElements.length; index += 1) {
     const userInput = formElements[index];
+    
     userInput.value = '';
     userInput.checked = false;
   }
+  
   textArea.value = '';
 };
 
 function enableSubmit() {
   const submitBtn = document.querySelector('#submit-btn');
   const agreement = document.querySelector('#agreement');
+  
   submitBtn.disabled = !agreement.checked;
 }
 
 const picker = new Pikaday({
   field: document.getElementById('date'),
   format: 'D/M/YYYY',
+  
   toString(date, format) {
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
+    
     return `${day}/${month}/${year}`;
   },
   parse(dateString, format) {
@@ -49,6 +55,7 @@ const picker = new Pikaday({
     const day = parseInt(parts[0], 10);
     const month = parseInt(parts[1], 10) - 1;
     const year = parseInt(parts[2], 10);
+    
     return new Date(year, month, day);
   }
 });
@@ -105,7 +112,8 @@ validate
 
 window.onload = function () {
   const clearBtn = document.querySelector('#clear-btn');
-  clearBtn.addEventListener('click', clearFields);
   const agreement = document.querySelector('#agreement');
+  
+  clearBtn.addEventListener('click', clearFields);
   agreement.addEventListener('change', enableSubmit);
 };
