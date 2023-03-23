fe57b913a621d801f62e1a46693ac82162372803
Gabriel Mendez Soares <gabrielmendezsoares@gmail.com>
2023-03-22 23:38:43 -0300

    v4.5

--- bloco-01-fundamentos/secao-01-unix-shell-e-git/dia-03-git-e-github-entendendo-os-comandos/exercicio-01.sh
+++ bloco-01-fundamentos/secao-01-unix-shell-e-git/dia-03-git-e-github-entendendo-os-comandos/exercicio-01.sh
@@ -6,7 +6,15 @@ git init
 touch README.md
 git add .
 git commit -m "[CREATE] README.md"
-git checkout -b newBranch
-echo -e "Lorem ipsum\nLorem ipsum\nLorem ipsum\nLorem ipsum" >README.md
+git checkout -b update-readme
+
+for index in 1 2 3 4 5; do
+  echo "${index} Lorem ipsum" >>README.md
+done
+
 git add .
 git commit -m "[CHANGE] README.md"
+git checkout master
+git merge update-readme
+cd ..
+rm -rf git_repository
