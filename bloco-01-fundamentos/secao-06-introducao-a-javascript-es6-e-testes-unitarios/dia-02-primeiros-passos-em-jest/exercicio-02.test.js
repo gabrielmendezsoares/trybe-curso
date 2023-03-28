e8b37fe83e419b00f77b88b771c53943758f408b
Gabriel Mendez Soares <gabrielmendezsoares@gmail.com>
2023-03-27 22:28:09 -0300

    Exercícios do dia

---
+++ bloco-01-fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-02.test.js
@@ -0,0 +1,9 @@
+const myRemove = require('./exercicio-01');
+
+describe('Testa a função myRemove', () => {
+  it('Deve retornar um array removendo o item correto', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
---
+++ bloco-01-fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercicio-08.test.js
@@ -0,0 +1,40 @@
+const techList = require('./exercicio-07.js');
+
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
+  });
+
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
+  });
+
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Gabriel')).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Gabriel'
+      },
+      {
+        tech: 'HTML',
+        name: 'Gabriel'
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Gabriel'
+      },
+      {
+        tech: 'Jest',
+        name: 'Gabriel'
+      },
+      {
+        tech: 'React',
+        name: 'Gabriel'
+      }
+    ]);
+  });
+
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Gabriel')).toBe('Vazio!');
+  });
+});
