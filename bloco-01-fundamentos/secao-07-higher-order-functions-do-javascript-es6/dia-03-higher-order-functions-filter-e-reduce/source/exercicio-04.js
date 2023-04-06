c40f285b874a5575cc59bae7616a38c90bc18754
Gabriel Mendez Soares <gabrielmendezsoares@gmail.com>
2023-04-06 11:28:37 -0300

    Exercícios do dia

---
+++ bloco-01-fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/source/exercicio-04.js
@@ -0,0 +1,43 @@
+const countries = require('../data/exercicio-03');
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha'
+];
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const pattern = /[Aa]/g;
+
+const getPopulation = () =>
+  countries.reduce((accumulator, country) =>
+    accumulator + country.population, 0);
+
+const getTotalArea = () =>
+  countries.reduce((accumulator, country) =>
+    accumulator + country.area, 0);
+
+const longestName = () =>
+  countries.reduce((accumulator, country) =>
+    (country.name.length > accumulator.name.length)
+      ? country
+      : accumulator);
+
+const countA = () =>
+  names.reduce((accumulator, name) =>
+    (name.match(pattern))
+      ? accumulator + name.match(pattern).length
+      : accumulator, 0);
+
+const studentAverage = () => students.map((student, index) => ({
+  name: student,
+  average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length)
+}
+));
+
+console.log(getPopulation());
+console.log(getTotalArea());
+console.log(longestName());
+console.log(countA());
+console.log(studentAverage());
