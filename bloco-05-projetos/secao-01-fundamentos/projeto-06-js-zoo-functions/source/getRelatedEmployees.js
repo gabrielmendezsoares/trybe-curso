b79c92a66b29fc7e5339061be73e3c017cd43973
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2023-04-13 19:20:09 -0300

    Refatoração

--- bloco-05-projetos/secao-01-fundamentos/projeto-06-js-zoo-functions/source/getRelatedEmployees.js
+++ bloco-05-projetos/secao-01-fundamentos/projeto-06-js-zoo-functions/source/getRelatedEmployees.js
@@ -1,23 +1,16 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-const isManager = (id) => {
-  const managerIds = [
-    '9e7d4524-363c-416a-8759-8aa7e50c0992',
-    'fdb2543b-5662-46a7-badc-93d960fdc0a8',
-    '0e7b460e-acf4-4e17-bcb3-ee472265db83',
-  ];
-
-  return managerIds.includes(id);
-};
+const isManager = (id) => employees
+  .some(({ managers }) => managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
-  if (isManager(managerId)) {
-    return data.employees
-      .filter((employee) => employee.managers.includes(managerId))
-      .map((employee) => `${employee.firstName} ${employee.lastName}`);
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
 
-  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  return employees
+    .filter(({ managers }) => managers.includes(managerId))
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
