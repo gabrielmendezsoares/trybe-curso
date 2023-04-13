84013c40ab61f1b19483695ec7081e0fbe04b537
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2023-04-13 20:38:29 -0300

    Refatoração

--- bloco-05-projetos/secao-01-fundamentos/projeto-06-js-zoo-functions/test/script-01.test.js
+++ bloco-05-projetos/secao-01-fundamentos/projeto-06-js-zoo-functions/test/script-01.test.js
@@ -1,61 +1,61 @@
-const handlerElephants = require('../src/handlerElephants');
+const handlerElephants = require('../source/script-06');
 
 describe('Tests for `HandlerElephants`', () => {
-  it('Test with `empty` parameter', () => {
+  test('Testing with `empty` parameter', () => {
     const actual = handlerElephants();
 
     expect(actual).toBeUndefined();
   });
 
-  it('Test with `0` parameter', () => {
+  test('Testing with `0` parameter', () => {
     const actual = handlerElephants(0);
     const expected = 'Parâmetro inválido, é necessário uma string';
 
     expect(actual).toBe(expected);
   });
 
-  it('Test with `test` parameter', () => {
+  test('Testing with `test` parameter', () => {
     const actual = handlerElephants('test');
 
     expect(actual).toBeNull();
   });
 
-  it('Test with `count` parameter', () => {
+  test('Testing with `count` parameter', () => {
     const actual = handlerElephants('count');
     const expected = 4;
 
     expect(actual).toBe(expected);
   });
 
-  it('Test with `names` parameter', () => {
+  test('Testing with `names` parameter', () => {
     const actual = handlerElephants('names');
     const expected = 'Jefferson';
 
     expect(actual).toContain(expected);
   });
 
-  it('Test with `averageAge` parameter', () => {
+  test('Testing with `averageAge` parameter', () => {
     const actual = handlerElephants('averageAge');
     const expected = 10.5;
 
     expect(actual).toBeCloseTo(expected);
   });
 
-  it('Test with `location` parameter', () => {
+  test('Testing with `location` parameter', () => {
     const actual = handlerElephants('location');
     const expected = 'NW';
 
     expect(actual).toBe(expected);
   });
 
-  it('Test with `popularity` parameter', () => {
+  test('Testing with `popularity` parameter', () => {
     const actual = handlerElephants('popularity');
     const expected = 5;
 
     expect(actual).toBe(expected);
   });
 
-  it('Test with `availability` parameter', () => {
+  it('Testing with `availability` parameter', () => {
     const actual = handlerElephants('availability');
     const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
 
