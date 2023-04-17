962b8fb733bc1346b4447b82b82af4e71b0cdbd5
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2023-04-17 11:23:14 -0300

    Refatoração

--- bloco-05-projetos/secao-01-fundamentos/projeto-06-js-zoo-functions/test/script-01.test.js
+++ bloco-05-projetos/secao-01-fundamentos/projeto-06-js-zoo-functions/test/script-01.test.js
@@ -1,13 +1,14 @@
 const getSpeciesByIds = require('../source/script-01');
 
-describe('1 - Implemente a função `getSpeciesByIds` que busca as espécies de animais por id', () => {
-  it('caso receba nenhum parâmetro, é necessário retornar um array vazio', () => {
+describe('01 - Tests for `getSpeciesByIds`', () => {
+  test('Testing with empty parameter', () => {
     const actual = getSpeciesByIds();
     const expected = [];
+
     expect(actual).toEqual(expected);
   });
 
-  it('ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id', () => {
+  test('Testing with unique Id parameter', () => {
     {
       const actual = getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
 
@@ -21,12 +22,13 @@ describe('1 - Implemente a função `getSpeciesByIds` que busca as espécies de 
           { name: 'Zena', sex: 'female', age: 12 },
           { name: 'Maxwell', sex: 'male', age: 15 },
           { name: 'Faustino', sex: 'male', age: 7 },
-          { name: 'Dee', sex: 'female', age: 14 },
-        ],
+          { name: 'Dee', sex: 'female', age: 14 }
+        ]
       }];
 
       expect(actual).toEqual(expected);
     }
+
     {
       const actual = getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed');
 
@@ -42,18 +44,18 @@ describe('1 - Implemente a função `getSpeciesByIds` que busca as espécies de 
           { name: 'Vicky', sex: 'female', age: 12 },
           { name: 'Clay', sex: 'male', age: 4 },
           { name: 'Arron', sex: 'male', age: 7 },
-          { name: 'Bernard', sex: 'male', age: 6 },
-        ],
+          { name: 'Bernard', sex: 'male', age: 6 }
+        ]
       }];
 
       expect(actual).toEqual(expected);
     }
   });
 
-  it('ao receber mais de um id, retorna um array com as espécies referentes aos ids', () => {
+  test('Testing with multiple Id parameters', () => {
     const actual = getSpeciesByIds(
       '0938aa23-f153-4937-9f88-4858b24d6bce',
-      'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+      'e8481c1d-42ea-4610-8e11-1752cfc05a46'
     );
 
     const expected = [{
@@ -66,8 +68,8 @@ describe('1 - Implemente a função `getSpeciesByIds` que busca as espécies de 
         { name: 'Zena', sex: 'female', age: 12 },
         { name: 'Maxwell', sex: 'male', age: 15 },
         { name: 'Faustino', sex: 'male', age: 7 },
-        { name: 'Dee', sex: 'female', age: 14 },
-      ],
+        { name: 'Dee', sex: 'female', age: 14 }
+      ]
     },
     {
       id: 'e8481c1d-42ea-4610-8e11-1752cfc05a46',
@@ -77,8 +79,8 @@ describe('1 - Implemente a função `getSpeciesByIds` que busca as espécies de 
       availability: ['Wednesday', 'Friday', 'Saturday', 'Tuesday'],
       residents: [
         { name: 'Shu', sex: 'female', age: 19 },
-        { name: 'Esther', sex: 'female', age: 17 },
-      ],
+        { name: 'Esther', sex: 'female', age: 17 }
+      ]
     }];
 
     expect(actual).toEqual(expected);
