d74d642c69d071e0f493648d6ed8b40230edf094
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2023-04-18 09:34:00 -0300

    Refatoração

--- bloco-01-fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/source/exercicio-01.js
+++ bloco-01-fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/source/exercicio-01.js
@@ -5,32 +5,32 @@ const valueA = 3;
 const valueB = 5;
 const sumNumber = (A, B) => A + B;
 const randomNumber = () => Math.random();
-const getHello = (string) => `Olá, ${string}!`;
-const getSpeed = (speed) => (speed >= 120) ? 'Você ultrapassou o limite de velocidade' : 'Você está na velocidade permitida';
+const getHello = (string) => `Hi, ${string}!`;
+const getSpeed = (speed) => (speed >= 120) ? 'Not allowed' : 'Allowed';
 const getName = (A, B) => `${A} ${B}`;
 const getAge = (A, B) => {
-  for (let idade = A; idade <= B; idade += 1) {
-    console.log('Idade:', idade);
+  for (let age = A; age <= B; age += 1) {
+    console.log('Age:', age);
   }
 };
 
-const pessoa = {
-  nome: 'Ninguém',
-  idade: 0
+const person = {
+  name: 'Nobody',
+  age: 0
 };
 
 getAge(1, 18);
 firstName = 'Gabriel';
 lastName = 'Soares';
-favoriteFood = 'Burger King';
-pessoa.nome = 'Gabriel';
-pessoa.idade = 18;
+favoriteFood = 'Sandwich';
+person.name = 'Gabriel';
+person.age = 18;
 console.log(getSpeed(180));
 console.log(getName('Ricardo', 'Batista'));
 console.log(getHello('Marcos Silva'));
-console.log(`Olá, ${firstName} ${lastName}!`);
-console.log(`Nome: ${pessoa.nome}`);
-console.log(`Idade: ${pessoa.idade}`);
-console.log(`Comida: ${favoriteFood}`);
-console.log(`Resultado da soma: ${sumNumber(valueA, valueB)}`);
-console.log(`Número aleatório: ${randomNumber()}`);
+console.log(`Hi, ${firstName} ${lastName}!`);
+console.log(`Name: ${person.name}`);
+console.log(`Age: ${person.age}`);
+console.log(`Food: ${favoriteFood}`);
+console.log(`Sum: ${sumNumber(valueA, valueB)}`);
+console.log(`Random: ${randomNumber()}`);
