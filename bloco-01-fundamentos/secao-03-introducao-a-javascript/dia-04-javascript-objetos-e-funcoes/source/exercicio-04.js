9255297c8b498a1dda36ddd8479fd5742992e07a
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2023-04-18 09:11:50 -0300

    Refatoração

--- bloco-01-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/source/exercicio-04.js
+++ bloco-01-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/source/exercicio-04.js
@@ -1,12 +1,12 @@
 const leitor = {
-  nome: 'Julia',
-  sobrenome: 'Pessoa',
-  idade: 21,
-  livrosFavoritos: [
+  name: 'Julia',
+  lastName: 'Pessoa',
+  age: 21,
+  favoriteBooks: [
     {
-      titulo: 'O Pior Dia de Todos',
-      autor: 'Daniela Kopsch',
-      editora: 'Tordesilhas'
+      title: 'O Pior Dia de Todos',
+      author: 'Daniela Kopsch',
+      editor: 'Tordesilhas'
     }
   ]
 };
@@ -14,10 +14,10 @@ const leitor = {
 leitor.livrosFavoritos.push(
   {
     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-    autor: 'JK Rowling',
-    editora: 'Rocco'
+    author: 'JK Rowling',
+    editor: 'Rocco'
   }
 );
 
-console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0].titulo}'`);
-console.log(`${leitor.nome} ${leitor.sobrenome} tem ${leitor.livrosFavoritos.length} livros favoritos`);
+console.log(`The favorite book of ${leitor.nome} ${leitor.sobrenome} is '${leitor.livrosFavoritos[0].titulo}'`);
+console.log(`${leitor.nome} ${leitor.sobrenome} has ${leitor.livrosFavoritos.length} favorite books`);
