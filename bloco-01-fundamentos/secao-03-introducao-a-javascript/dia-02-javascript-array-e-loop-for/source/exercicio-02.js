cbbf494a65a720020cce65c812a6da7c6cc9baab
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2023-04-17 22:19:59 -0300

    Refatoração

--- bloco-01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/source/exercicio-02.js
+++ bloco-01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/source/exercicio-02.js
@@ -1,4 +1,4 @@
-const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+const groceryList = ['Rice', 'Beans', 'Lettuce', 'Watermelon'];
 const names = ['João', 'Maria', 'Antônio', 'Margarida'];
 
 for (let index = 0; index < groceryList.length; index += 1) {
