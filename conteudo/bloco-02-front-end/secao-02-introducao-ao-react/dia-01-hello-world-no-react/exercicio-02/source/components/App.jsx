b575633913c370fb37b68b5905064da97f879033
gabrielmendezsoares <gabrielmendezsoares@gmail.com>
2023-05-22 22:36:03 -0300

    [Chore]: Daily exercise

--- conteudo/bloco-02-front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/exercicio-02/source/components/App.jsx
+++ conteudo/bloco-02-front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/exercicio-02/source/components/App.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <>
         <h1>Exercise - Frontend content</h1>
--- conteudo/bloco-02-front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/exercicio-02/source/components/Footer.jsx
+++ conteudo/bloco-02-front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/exercicio-02/source/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
-  render() {
+  render () {
     return (
       <footer data-testid="footer">
         <h3>E isso é só o começo...</h3>
